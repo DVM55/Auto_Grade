dc040fbafbd196108ac3c6651e4420d0d9499d8f
manhdzai1105 <duongvanmanh11052003@gmail.com>
2026-02-23 21:54:06 +0700

    config minio

---
+++ src/main/java/com/example/Auto_Grade/integration/minio/MinioProps.java
@@ -0,0 +1,20 @@
+package com.example.Auto_Grade.integration.minio;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "minio")
+public class MinioProps {
+    private String endpoint;
+    private String accessKey;
+    private String secretKey;
+    private String bucket;
+    private String keyPrefix ;
+    private int presignExpirySeconds;
+    private boolean makeBucketPublic;
+}
