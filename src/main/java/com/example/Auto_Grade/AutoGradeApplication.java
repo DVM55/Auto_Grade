1538111afbb841c8c868f1077b739406b3190430
manhdzai1105 <duongvanmanh11052003@gmail.com>
2026-02-23 21:25:56 +0700

    init project

---
+++ src/main/java/com/example/Auto_Grade/AutoGradeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Auto_Grade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoGradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoGradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Auto_Grade/AutoGradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Auto_Grade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoGradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
